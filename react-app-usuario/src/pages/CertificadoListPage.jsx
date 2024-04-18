ecdc8eef38b01fc393579df14d30583dc04cfdaa
damian <adamodamian6@gmail.com>
2024-04-18 18:21:28 -0300

    pdf en certificados y dando formato al formulario

--- react-app-usuario/src/pages/CertificadoListPage.jsx
+++ react-app-usuario/src/pages/CertificadoListPage.jsx
@@ -3,6 +3,7 @@ import { useAuth } from '../auth/hooks/useAuth';
 import { useCertificados } from '../hooks/useCertificados';
 import { CertificadoModalForm } from '../components/certificado/CertificadoModalForm';
 import { CertificadoList } from '../components/certificado/CertificadoList';
+import { NavLink } from 'react-router-dom';
 export const CertificadoListPage = () => {
   const {
     certificados,
@@ -20,18 +21,13 @@ useEffect(() => {
 return (
     <>
 
-        {!visibleForm ||
-            <CertificadoModalForm/>}
+     
         <div className="container my-4">
             <h2>Laboratorio app</h2>
             <div className="row">
                 <div className="col">
-                    {(visibleForm || !login.isAdmin) || <button
-                        className="btn btn-primary my-2"
-                        onClick={handlerOpenForm}>
-                        Nuevo Certificado
-                    </button>}
-
+               
+<NavLink className="btn btn-primary m-2"  to={"/certificados/registrar"} >Nuevo Certificado</NavLink>
                     {
                         certificados.length === 0
                             ? <div className="alert alert-warning">No hay certificados en el sistema!</div>
